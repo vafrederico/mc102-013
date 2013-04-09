2e935152a36d4be257437703188e88e4d13fd0ca
Vinicius Frederico <vafrederico@gmail.com>
2013-04-09 18:56:17 -0300

    strlen para gets

--- lab05b/main.c
+++ lab05b/main.c
@@ -25,22 +25,35 @@ int main (){
 	int minusculoParaMaiusculo = 'a' - 'A';
 	int tamP, tamM, i, j, pos, soma, achou, error;
 
+	for( i = 0; i < 140; i++ ){ // Reseta m e p
+		m[i] = '\0';
+		p[i] = '\0';
+	}
+
 	/* Lê p */
 	gets(p);
 
-	/* Tamanho de p */
-	tamP = strlen(p);
+	/* Lê m */
+	gets(m);
 	
+	/* tamanho = -1 para saber se já foi determinado */
+	tamP = -1;
+	tamM = -1;
+	for( i = 0; i < 140 ; i++){
+
+		if(m[i] == '\0' && tamM == -1) // se é um caractere não escrito por cima de \0 e não foi determinado o tamanho
+			tamM = i;
+	
+		if(p[i] == '\0' && tamP == -1) // se é um caractere não escrito por cima de \0 e não foi determinado o tamanho
+			tamP = i;
+
+	}
+
 	/* Converte toda minúscula em p (p[i] > 'Z') para maiúscula para facilitar */
 	for( i = 0; i < tamP; i++)
 		if(p[i] > 'Z')
 			p[i] = p[i] - minusculoParaMaiusculo;
 
-	/* Lê m */
-	gets(m);
-	/* Tamanho de m */
-	tamM = strlen(m);
-	
 	/* no começo, o bloco de texto a se comparar está no 0, n se achou nenhum jeito */
 	pos = 0;
 	achou = 0;
